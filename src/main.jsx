fb7f8a2167c9d1250975c58fa6d83cd256f7341a
Rahul Shukla <rahulshukla0902@gmail.com>
2024-06-30 17:59:07 +0530

    IPhone 3D Website ThreeJS GSAP Project

---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://0326f824b2de6d2d07e5504d5dfac320@o4507519667798016.ingest.us.sentry.io/4507519669633024",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
